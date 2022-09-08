0bca770aa916624d2c4535338b9a63b3c5141477
Felix Ishabiyi <felixishabiyi@gmail.com>
2022-09-07 18:20:14 -1200

    adding my temperature converter

---
+++ Temp Converter.py
@@ -0,0 +1,18 @@
+import math
+
+# temp to be converted to fahranheit 
+
+c = float(input('Enter your value of temp in Celcius here: '))
+
+# To calculate to Fahranheit
+
+Fah = (c * 1.8) + 32
+
+print (Fah)
+
+# temp to be converted to celcius
+
+f = float(input('Enter your value of temp in fahrenheit here: '))
+Cel= ( F - 32) * 1.8
+
+print(Cel)
